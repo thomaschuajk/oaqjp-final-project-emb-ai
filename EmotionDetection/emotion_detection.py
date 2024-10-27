deebe1f58b78cfdce3a05d4074519de3ced37cb9
thomaschuajk <thomaschuajk@gmail.com>
2024-10-27 11:30:26 -0400

    updated copy for submission

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,14 +6,26 @@ def emotion_detector(text_to_analyse):
     HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     INPUT_JSON = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL,json=INPUT_JSON, headers=HEADERS)
-    json_response = json.loads(response.text)
-    formatted_response = json_response["emotionPredictions"][0]['emotion']
-    emotions_list = []
-    scores_list = []
-    for emotion, score in formatted_response.items():
-        emotions_list.append(emotion)
-        scores_list.append(score)
-    highest_score_indice = scores_list.index(max(scores_list))
-    name_dominant_emotion = emotions_list[highest_score_indice]
-    formatted_response['dominant_emotion'] = name_dominant_emotion
-    return formatted_response
+        
+    if response.status_code == 200:
+        json_response = json.loads(response.text)
+        formatted_response = json_response["emotionPredictions"][0]['emotion']
+        emotions_list = []
+        scores_list = []
+        for emotion, score in formatted_response.items():
+            emotions_list.append(emotion)
+            scores_list.append(score)
+            highest_score_indice = scores_list.index(max(scores_list))
+            name_dominant_emotion = emotions_list[highest_score_indice]
+        formatted_response['dominant_emotion'] = name_dominant_emotion   
+        return formatted_response        
+    elif response.status_code == 400:
+        formatted_response = {
+                               "anger":None,
+                               "disgust":None,
+                               "fear":None,
+                               "joy":None,
+                               "sadness":None,
+                               "dominant_emotion":None
+                             }
+        return formatted_response
--- server.py
+++ server.py
@@ -1,18 +1,29 @@
+'''This is code implementation for backend service'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Sentiment Analyzer")
+app = Flask("Emotion Predictor")
 
 @app.route('/')
 def render_index_page():
-    render_template('/templates/index.html')
+    '''This is the function for rendering the landing page'''
+    return render_template('index.html')
 
 @app.route('/emotionDetector')
 def sent_detector():
+    '''This is the function for returning the emotion detection analysis'''
     text_to_detect = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_detect)
-    system_response = f"For the given statement, the system response is 'anger': ${response['anger']}, 'disgust': ${response['disgust']}, 'fear': ${response['fear']}, 'joy': ${response['joy']} and 'sadness': ${response['sadness']}. The dominant emotion is ${response['dominant_emotion']}."
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    system_response = f"For the given statement, the system response is \
+                    'anger': {response['anger']}, \
+                    'disgust': {response['disgust']}, \
+                    'fear': {response['fear']}, \
+                    'joy': {response['joy']} \
+                    and 'sadness': {response['sadness']}. \
+                    The dominant emotion is {response['dominant_emotion']}."
     return system_response
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0',port=5000)
